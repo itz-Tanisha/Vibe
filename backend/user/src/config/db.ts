085158515778d5a58b419e1875ce5013c3e6a56e
tanisha <tanisha@perusal.in>
2026-01-02 17:03:47 +0530

    Db Setup done

---
+++ backend/user/src/config/db.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectToDB = async () => {
+
+    const url = process.env.MONGO_URI;
+
+    // If no url found throw an error 
+    if (!url) {
+        throw new Error("MONGO_URL not defined in enviroment variables.")
+    }
+
+    try {
+        await mongoose.connect(url, {
+            dbName:"Vibe RealTime ChatApp"
+        });
+
+        console.log("MongoDB Connected !");
+        
+    }
+    catch(err){
+        console.log('Failed to connect to MongoDB', err);
+        //???
+        process.exit(1);
+    }   
+
+}
+
+export default connectToDB;
+
+
--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectToDB from "./config/db.js";
+
+
+dotenv.config();
+connectToDB();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
     console.log(`User service running on port ${port}`);
